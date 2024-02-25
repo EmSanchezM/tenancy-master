6bc3b25a9cd5ffffac74548babc95c9e8f65883a
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2024-02-25 10:01:05 -0600

    feat: add sidebar temporaly

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Sidebar from "../common/navigation/sidebar.vue";
+
+const menu = [
+  { name: "Restaurantes", to: "/restaurants" },
+  { name: "Usuarios", to: "/users" },
+  { name: "Configuraciones", to: "/settings" },
+];
+</script>
+
+<template>
+  <main class="flex justify-center w-full min-h-screen bg-gray-200">
+    <Sidebar />
+    <section class="flex min-h-screen flex-1 flex-col bg-gray-800">
+      <nav class="flex items-center gap-x-6">NAVBAR</nav>
+      <slot />
+    </section>
+  </main>
+</template>
