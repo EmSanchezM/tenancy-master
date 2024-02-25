fe2f514dc1312513fb1ceaa68f991c3a347084df
Elvin Sanchez <elvinmoises.sanchez@gmail.com>
2024-02-25 08:48:12 -0600

    feat: add auth layout

---
+++ layouts/auth.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="w-full max-w-md p-8 space-y-3 rounded-xl shadow-md">
+      <slot />
+    </div>
+  </div>
+</template>
